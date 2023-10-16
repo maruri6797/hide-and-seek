b8238231b9e2317ca637699281273b24010a6941
maruri6797 <138319041+maruri6797@users.noreply.github.com>
2023-10-16 07:11:06 +0000

    Update adminreport

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -6,16 +6,19 @@ class Admin::ReportsController < ApplicationController
   def show
     @report = Report.find(params[:id])
   end
-  
+
   def update
     @report = Report.find(params[:id])
     if @report.update(report_params)
       redirect_to request.referer, notice: "更新しました。"
+    else
+      flash.now[:alert] = "更新に失敗しました。"
+      render :show
     end
   end
-  
+
   private
-  
+
   def report_params
     params.require(:report).permit(:is_checked)
   end
