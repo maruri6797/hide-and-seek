8bac5a5041b8a7cf0d0adcefcd320263380d0f42
maruri6797 <138319041+maruri6797@users.noreply.github.com>
2023-11-06 04:02:47 +0000

    Fix

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def active_for_authentication?
-    super && current_user.is_deleted == true
+    if current_user.is_deleted == true
     redirect_to new_user_registration_path
+    end
   end
 end
