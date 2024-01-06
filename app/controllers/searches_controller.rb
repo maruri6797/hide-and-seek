a0ddf744b4f48536d73cce03c59f59079e614125
maruri6797 <138319041+maruri6797@users.noreply.github.com>
2024-01-06 05:11:05 +0000

    Finish haml

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -12,14 +12,14 @@ class SearchesController < ApplicationController
 
   def result
     @posts = Post.where.not(status: 2)
-    @keywords = params[:q][:title_or_text_cont].split(/[\p{blank}\s]+/)
-    grouping_hash = @keywords.reduce({}){|hash, word| hash.merge(word => { title_or_text_cont: word })}
-    @q = @posts.ransack({ combinator: 'and', groupings: grouping_hash })
     if params[:tag_id].present?
       @tag = Tag.find(params[:tag_id])
       @posts = @posts.joins(:post_tags).where(post_tags: {tag_id: @tag.id})
     else
       redirect_to search_path if @keyword == ""
+      @keywords = params[:q][:title_or_text_cont].split(/[\p{blank}\s]+/)
+      grouping_hash = @keywords.reduce({}){|hash, word| hash.merge(word => { title_or_text_cont: word })}
+      @q = @posts.ransack({ combinator: 'and', groupings: grouping_hash })
       @posts = @q.result(distinct: true)
     end
   end
